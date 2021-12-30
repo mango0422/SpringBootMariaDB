8f1046dcca40b5510fd791926acbc44a0eed97b3
Taeyeol Seo <taeeyoul77@gmail.com>
2021-12-30 18:11:46 +0900

    2021-12-30

--- src/main/java/com/example/demo/SpringBootSampleApplication.java
+++ src/main/java/com/example/demo/SpringBootSampleApplication.java
@@ -3,7 +3,6 @@ package com.example.demo;
 // import com.example.demo.controller.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
